5e815db9daf499faf8739544b13e8e464a7609ab
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 02:40:07 +0300

    Update MainViewModel.cs

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -6,31 +6,15 @@ namespace DS3InputMaster.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        private string _statusMessage = "Готов к работе";
-        
         public MainViewModel()
         {
-            AvailableProfiles = new ObservableCollection<string> { "Default", "PvP", "Magic" };
-            LogMessages = new ObservableCollection<string> { "Система инициализирована" };
-            
-            StartCommand = ReactiveCommand.Create(StartSystem);
+            StartCommand = ReactiveCommand.Create(() => { });
         }
-
-        public string StatusMessage
-        {
-            get => _statusMessage;
-            set => this.RaiseAndSetIfChanged(ref _statusMessage, value);
-        }
-
-        public ObservableCollection<string> AvailableProfiles { get; }
-        public ObservableCollection<string> LogMessages { get; }
+        
+        public string StatusMessage => "Готов к работе";
+        public ObservableCollection<string> AvailableProfiles { get; } = new() { "Default" };
+        public ObservableCollection<string> LogMessages { get; } = new() { "Запущено" };
         public ICommand StartCommand { get; }
-
-        private void StartSystem()
-        {
-            StatusMessage = "Система запущена!";
-            LogMessages.Add("Система управления активирована");
-        }
     }
 
     public abstract class ViewModelBase : ReactiveUI.ReactiveObject { }
