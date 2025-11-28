df10caaa16c7b9aab931825d434204099348c2d3
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 02:13:45 +0300

    Create BoolInvertConverter.cs

---
+++ Converters/BoolInvertConverter.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace DS3InputMaster.Converters
+{
+    public class BoolInvertConverter : IValueConverter
+    {
+        public static BoolInvertConverter Instance { get; } = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool boolValue ? !boolValue : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool boolValue ? !boolValue : value;
+        }
+    }
+}
