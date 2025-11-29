fef11c720a63b7638eef7a000c5425edfa422584
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 12:17:26 +0300

    Update MainViewModel.cs

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -6,15 +6,54 @@ namespace DS3InputMaster.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        private bool _isCapturingInput;
+        private string _currentGameState = "Exploring";
+        private string _selectedProfile = "Default";
+        private string _statusMessage = "Готов к работе";
+
         public MainViewModel()
         {
             StartCommand = ReactiveCommand.Create(() => { });
+            StopCommand = ReactiveCommand.Create(() => { });
+            CalibrateCommand = ReactiveCommand.Create(() => { });
+            CreateProfileCommand = ReactiveCommand.Create(() => { });
+            SaveProfileCommand = ReactiveCommand.Create(() => { });
+            DeleteProfileCommand = ReactiveCommand.Create(() => { });
         }
         
-        public string StatusMessage => "Готов к работе";
-        public ObservableCollection<string> AvailableProfiles { get; } = new() { "Default" };
-        public ObservableCollection<string> LogMessages { get; } = new() { "Запущено" };
+        public string StatusMessage 
+        {
+            get => _statusMessage;
+            set => this.RaiseAndSetIfChanged(ref _statusMessage, value);
+        }
+
+        public string CurrentGameState 
+        {
+            get => _currentGameState;
+            set => this.RaiseAndSetIfChanged(ref _currentGameState, value);
+        }
+
+        public bool IsCapturingInput 
+        {
+            get => _isCapturingInput;
+            set => this.RaiseAndSetIfChanged(ref _isCapturingInput, value);
+        }
+
+        public string SelectedProfile 
+        {
+            get => _selectedProfile;
+            set => this.RaiseAndSetIfChanged(ref _selectedProfile, value);
+        }
+
+        public ObservableCollection<string> AvailableProfiles { get; } = new() { "Default", "PvP", "Magic" };
+        public ObservableCollection<string> LogMessages { get; } = new() { "Система инициализирована" };
+        
         public ICommand StartCommand { get; }
+        public ICommand StopCommand { get; }
+        public ICommand CalibrateCommand { get; }
+        public ICommand CreateProfileCommand { get; }
+        public ICommand SaveProfileCommand { get; }
+        public ICommand DeleteProfileCommand { get; }
     }
 
     public abstract class ViewModelBase : ReactiveUI.ReactiveObject { }
