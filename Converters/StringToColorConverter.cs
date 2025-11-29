e28d0697fbf7f888e2903d7d7d51d1a63fc53577
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 03:56:11 +0300

    Update StringToColorConverter.cs

--- Converters/StringToColorConverter.cs
+++ Converters/StringToColorConverter.cs
@@ -17,21 +17,21 @@ namespace DS3InputMaster.Converters
             {
                 return gameState switch
                 {
-                    GameState.Exploring => Brushes.Green,
-                    GameState.InCombat => Brushes.Red,
-                    GameState.BossFight => Brushes.DarkRed,
-                    GameState.Aiming => Brushes.Orange,
-                    GameState.BowAiming => Brushes.OrangeRed,
-                    GameState.Parrying => Brushes.Yellow,
-                    GameState.Rolling => Brushes.YellowGreen,
-                    GameState.MenuNavigation => Brushes.Blue,
-                    GameState.Dialog => Brushes.Purple,
-                    GameState.Dead => Brushes.Gray,
-                    GameState.Loading => Brushes.LightGray,
-                    _ => Brushes.Black
+                    GameState.Exploring => Avalonia.Media.Brushes.Green,
+                    GameState.InCombat => Avalonia.Media.Brushes.Red,
+                    GameState.BossFight => Avalonia.Media.Brushes.DarkRed,
+                    GameState.Aiming => Avalonia.Media.Brushes.Orange,
+                    GameState.BowAiming => Avalonia.Media.Brushes.OrangeRed,
+                    GameState.Parrying => Avalonia.Media.Brushes.Yellow,
+                    GameState.Rolling => Avalonia.Media.Brushes.YellowGreen,
+                    GameState.MenuNavigation => Avalonia.Media.Brushes.Blue,
+                    GameState.Dialog => Avalonia.Media.Brushes.Purple,
+                    GameState.Dead => Avalonia.Media.Brushes.Gray,
+                    GameState.Loading => Avalonia.Media.Brushes.LightGray,
+                    _ => Avalonia.Media.Brushes.Black
                 };
             }
-            return Brushes.Black;
+            return Avalonia.Media.Brushes.Black;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
