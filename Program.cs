1e0edcd6034d369dd646881bfcdea317ba393600
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 12:21:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 using Avalonia;
 using System;
+using System.Threading;
 
 namespace DS3InputMaster
 {
     internal class Program
     {
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            try
+            {
+                BuildAvaloniaApp()
+                    .StartWithClassicDesktopLifetime(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex}");
+                Console.ReadLine();
+            }
+        }
 
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
