78aa89bc34a5fa982992692b0d4f50d6c968b840
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 03:33:40 +0300

    Update App.axaml.cs

--- App.axaml.cs
+++ App.axaml.cs
@@ -2,31 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using DS3InputMaster.Views;
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Markup.Xaml;
-using DS3InputMaster.Views;
 
-namespace DS3InputMaster
-{
-    public class App : Avalonia.Application 
-    {
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-
-        public override void OnFrameworkInitializationCompleted()
-        {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow();
-            }
-
-            base.OnFrameworkInitializationCompleted();
-        }
-    }
-}
 namespace DS3InputMaster
 {
     public class App : Application
