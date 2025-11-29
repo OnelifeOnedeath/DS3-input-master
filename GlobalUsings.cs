bc92062c57b2df26e0ee09c3ec8a7a7ff43881d5
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 03:26:28 +0300

    Create GlobalUsings.cs

---
+++ GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Drawing;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using WindowsInput;
+global using WindowsInput.Native;
