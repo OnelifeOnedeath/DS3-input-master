e751658f01ad3bd98efc8dfcee46b62e9864f96c
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 12:45:26 +0300

    Update MainWindow.axaml.cs

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,14 +1,19 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using DS3InputMaster.ViewModels;
 
 namespace DS3InputMaster.Views
 {
-    public class MainWindow : Window
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            DataContext = new MainViewModel();
         }
 
         private void InitializeComponent()
