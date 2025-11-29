18112665d007fb2748b1f891eb179f1e8832f0dc
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 04:11:58 +0300

    Update GamepadEmulator.cs

--- Core/Emulation/GamepadEmulator.cs
+++ Core/Emulation/GamepadEmulator.cs
@@ -6,31 +6,36 @@ using WindowsInput.Native;
 
 namespace DS3InputMaster.Core.Emulation
 {
-    public class GamepadEmulator
+    public class GamepadEmulator : IDisposable
     {
         private readonly InputSimulator _inputSimulator = new InputSimulator();
         private Thread _emulationThread;
         private bool _isRunning;
         private GamepadOutput _currentOutput;
+        private readonly object _lockObject = new object();
 
-        public void Start()
+        public void StartEmulation()
         {
             if (_isRunning) return;
             
             _isRunning = true;
             _emulationThread = new Thread(EmulationLoop);
+            _emulationThread.IsBackground = true;
             _emulationThread.Start();
         }
 
-        public void Stop()
+        public void StopEmulation()
         {
             _isRunning = false;
             _emulationThread?.Join(1000);
         }
 
-        public void SetOutput(GamepadOutput output)
+        public void UpdateState(GamepadOutput output)
         {
-            _currentOutput = output;
+            lock (_lockObject)
+            {
+                _currentOutput = output;
+            }
         }
 
         private void EmulationLoop()
@@ -39,11 +44,21 @@ namespace DS3InputMaster.Core.Emulation
             {
                 try
                 {
-                    if (_currentOutput != null)
+                    GamepadOutput output;
+                    lock (_lockObject)
                     {
-                        EmulateGamepad(_currentOutput);
+                        output = _currentOutput;
                     }
-                    Thread.Sleep(16); // ~60Hz
+
+                    if (output != null)
+                    {
+                        EmulateGamepad(output);
+                    }
+                    Thread.Sleep(16);
+                }
+                catch (ThreadAbortException)
+                {
+                    break;
                 }
                 catch (Exception ex)
                 {
@@ -54,18 +69,26 @@ namespace DS3InputMaster.Core.Emulation
 
         private void EmulateGamepad(GamepadOutput output)
         {
-            // Emulate movement
-            if (output.Movement.X != 0 || output.Movement.Y != 0)
-            {
-                var normalized = output.Movement.Normalized();
-                // Convert to gamepad input
-            }
+            // Emulate movement as WASD or arrow keys
+            if (output.Movement.X > 0.1f)
+                _inputSimulator.Keyboard.KeyDown(VirtualKeyCode.VK_D);
+            else
+                _inputSimulator.Keyboard.KeyUp(VirtualKeyCode.VK_D);
 
-            // Emulate camera
-            if (output.Camera.X != 0 || output.Camera.Y != 0)
-            {
-                // Convert mouse movement to right stick
-            }
+            if (output.Movement.X < -0.1f)
+                _inputSimulator.Keyboard.KeyDown(VirtualKeyCode.VK_A);
+            else
+                _inputSimulator.Keyboard.KeyUp(VirtualKeyCode.VK_A);
+
+            if (output.Movement.Y > 0.1f)
+                _inputSimulator.Keyboard.KeyDown(VirtualKeyCode.VK_W);
+            else
+                _inputSimulator.Keyboard.KeyUp(VirtualKeyCode.VK_W);
+
+            if (output.Movement.Y < -0.1f)
+                _inputSimulator.Keyboard.KeyDown(VirtualKeyCode.VK_S);
+            else
+                _inputSimulator.Keyboard.KeyUp(VirtualKeyCode.VK_S);
 
             // Emulate actions
             foreach (var action in output.Actions)
@@ -84,8 +107,51 @@ namespace DS3InputMaster.Core.Emulation
                 case GameAction.HeavyAttack:
                     _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_K);
                     break;
-                // Add other actions...
+                case GameAction.Block:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_L);
+                    break;
+                case GameAction.Parry:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_O);
+                    break;
+                case GameAction.UseItem:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_Q);
+                    break;
+                case GameAction.Interact:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_E);
+                    break;
+                case GameAction.Roll:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_SPACE);
+                    break;
+                case GameAction.Jump:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_SPACE);
+                    break;
+                case GameAction.Menu:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.ESCAPE);
+                    break;
+                case GameAction.SwitchSpell:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_X);
+                    break;
+                case GameAction.SwitchItem:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_Z);
+                    break;
+                case GameAction.TwoHand:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_Y);
+                    break;
+                case GameAction.LockOn:
+                    _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_F);
+                    break;
+                case GameAction.MoveForward:
+                case GameAction.MoveBackward:
+                case GameAction.MoveLeft:
+                case GameAction.MoveRight:
+                    // These are handled by movement system
+                    break;
             }
+        }
+
+        public void Dispose()
+        {
+            StopEmulation();
         }
     }
 
