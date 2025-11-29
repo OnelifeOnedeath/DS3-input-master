9af589957e09f03d6e8100a9668e545f8edf5687
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-29 03:43:48 +0300

    Update GamepadEmulator.cs

--- Core/Emulation/GamepadEmulator.cs
+++ Core/Emulation/GamepadEmulator.cs
@@ -1,17 +1,11 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
-using WindowsInput;
-using WindowsInput.Native;
 
 namespace DS3InputMaster.Core.Emulation
 {
-    /// <summary>
-    /// Эмулятор ввода геймпада для Dark Souls 3
-    /// </summary>
     public class GamepadEmulator : IDisposable
     {
-        private readonly InputSimulator _inputSimulator;
         private readonly XInputState _currentState;
         private readonly XInputState _previousState;
         private bool _isEmulating;
@@ -20,7 +14,6 @@ namespace DS3InputMaster.Core.Emulation
 
         public GamepadEmulator()
         {
-            _inputSimulator = new InputSimulator();
             _currentState = new XInputState();
             _previousState = new XInputState();
         }
@@ -49,23 +42,19 @@ namespace DS3InputMaster.Core.Emulation
         {
             lock (_stateLock)
             {
-                // Сохраняем предыдущее состояние
                 _previousState.CopyFrom(_currentState);
 
-                // Обновляем текущее состояние
                 _currentState.LeftThumbX = ScaleAnalogValue(output.Movement.X);
                 _currentState.LeftThumbY = ScaleAnalogValue(output.Movement.Y);
                 _currentState.RightThumbX = ScaleAnalogValue(output.Camera.X);
                 _currentState.RightThumbY = ScaleAnalogValue(output.Camera.Y);
 
-                // Обновляем кнопки на основе действий
                 UpdateButtonsFromActions(output.Actions);
             }
         }
 
         private void UpdateButtonsFromActions(IReadOnlyList<GameAction> actions)
         {
-            // Сбрасываем все кнопки
             _currentState.Buttons = 0;
 
             foreach (var action in actions)
@@ -90,34 +79,12 @@ namespace DS3InputMaster.Core.Emulation
                     case GameAction.Jump:
                         _currentState.Buttons |= XInputButtons.A;
                         break;
-                    case GameAction.Sprint:
-                        _currentState.Buttons |= XInputButtons.LeftThumb;
-                        break;
-                    case GameAction.LockOn:
-                        _currentState.Buttons |= XInputButtons.RightThumb;
-                        break;
-                    case GameAction.Menu:
-                        _currentState.Buttons |= XInputButtons.Start;
-                        break;
-                    case GameAction.SwitchRightHand:
-                        _currentState.Buttons |= XInputButtons.DPadRight;
-                        break;
-                    case GameAction.SwitchLeftHand:
-                        _currentState.Buttons |= XInputButtons.DPadLeft;
-                        break;
-                    case GameAction.SwitchSpell:
-                        _currentState.Buttons |= XInputButtons.DPadUp;
-                        break;
-                    case GameAction.SwitchItem:
-                        _currentState.Buttons |= XInputButtons.DPadDown;
-                        break;
                 }
             }
         }
 
         private short ScaleAnalogValue(float value)
         {
-            // Преобразуем из [-1, 1] в [-32768, 32767]
             return (short)(value * (value >= 0 ? 32767 : 32768));
         }
 
@@ -129,67 +96,25 @@ namespace DS3InputMaster.Core.Emulation
                 {
                     lock (_stateLock)
                     {
-                        // Эмулируем изменения состояния
                         EmulateStateChanges();
                     }
-
-                    Thread.Sleep(8); // ~120Hz
+                    Thread.Sleep(8);
                 }
-                catch (ThreadAbortException)
-                {
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    // Логирование ошибок эмуляции
-                    Console.WriteLine($"Emulation error: {ex.Message}");
-                }
+                catch (ThreadAbortException) { break; }
+                catch (Exception ex) { }
             }
         }
 
         private void EmulateStateChanges()
         {
-            // Эмулируем нажатия кнопок
             EmulateButtonPresses();
-            
-            // Эмулируем движение аналоговых стиков
             EmulateThumbMovement();
-            
-            // Эмулируем триггеры
             EmulateTriggers();
         }
 
-        private void EmulateButtonPresses()
-        {
-            var pressedButtons = _currentState.Buttons & ~_previousState.Buttons;
-            var releasedButtons = _previousState.Buttons & ~_currentState.Buttons;
-
-            // Эмулируем нажатия кнопок через виртуальные клавиши
-            // (здесь будет интеграция с виртуальным геймпадом)
-        }
-
-        private void EmulateThumbMovement()
-        {
-            // Эмулируем движение левого стика (передвижение)
-            if (_currentState.LeftThumbX != _previousState.LeftThumbX ||
-                _currentState.LeftThumbY != _previousState.LeftThumbY)
-            {
-                // Отправляем обновление позиции левого стика
-            }
-
-            // Эмулируем движение правого стика (камера)
-            if (_currentState.RightThumbX != _previousState.RightThumbX ||
-                _currentState.RightThumbY != _previousState.RightThumbY)
-            {
-                // Отправляем обновление позиции правого стика
-            }
-        }
-
-        private void EmulateTriggers()
-        {
-            // Эмулируем нажатие триггеров
-            // (можно использовать для сильных атак или специальных действий)
-        }
+        private void EmulateButtonPresses() { }
+        private void EmulateThumbMovement() { }
+        private void EmulateTriggers() { }
 
         private void ResetState()
         {
@@ -206,9 +131,6 @@ namespace DS3InputMaster.Core.Emulation
         }
     }
 
-    /// <summary>
-    /// Состояние геймпада XInput
-    /// </summary>
     public class XInputState
     {
         public short LeftThumbX { get; set; }
@@ -221,53 +143,27 @@ namespace DS3InputMaster.Core.Emulation
 
         public void Reset()
         {
-            LeftThumbX = 0;
-            LeftThumbY = 0;
-            RightThumbX = 0;
-            RightThumbY = 0;
-            LeftTrigger = 0;
-            RightTrigger = 0;
-            Buttons = 0;
+            LeftThumbX = 0; LeftThumbY = 0; RightThumbX = 0; RightThumbY = 0;
+            LeftTrigger = 0; RightTrigger = 0; Buttons = 0;
         }
 
         public void CopyFrom(XInputState other)
         {
-            LeftThumbX = other.LeftThumbX;
-            LeftThumbY = other.LeftThumbY;
-            RightThumbX = other.RightThumbX;
-            RightThumbY = other.RightThumbY;
-            LeftTrigger = other.LeftTrigger;
-            RightTrigger = other.RightTrigger;
+            LeftThumbX = other.LeftThumbX; LeftThumbY = other.LeftThumbY;
+            RightThumbX = other.RightThumbX; RightThumbY = other.RightThumbY;
+            LeftTrigger = other.LeftTrigger; RightTrigger = other.RightTrigger;
             Buttons = other.Buttons;
         }
     }
 
-    /// <summary>
-    /// Кнопки геймпада XInput
-    /// </summary>
     [Flags]
     public enum XInputButtons : ushort
     {
-        None = 0,
-        DPadUp = 0x0001,
-        DPadDown = 0x0002,
-        DPadLeft = 0x0004,
-        DPadRight = 0x0008,
-        Start = 0x0010,
-        Back = 0x0020,
-        LeftThumb = 0x0040,
-        RightThumb = 0x0080,
-        LB = 0x0100,
-        RB = 0x0200,
-        A = 0x1000,
-        B = 0x2000,
-        X = 0x4000,
-        Y = 0x8000
+        None = 0, DPadUp = 0x0001, DPadDown = 0x0002, DPadLeft = 0x0004, DPadRight = 0x0008,
+        Start = 0x0010, Back = 0x0020, LeftThumb = 0x0040, RightThumb = 0x0080,
+        LB = 0x0100, RB = 0x0200, A = 0x1000, B = 0x2000, X = 0x4000, Y = 0x8000
     }
 
-    /// <summary>
-    /// Выходные данные для эмуляции геймпада
-    /// </summary>
     public class GamepadOutput
     {
         public Vector2 Movement { get; set; }
@@ -277,14 +173,11 @@ namespace DS3InputMaster.Core.Emulation
         public float RightTrigger { get; set; }
     }
 
-    // Временные заглушки для отсутствующих типов
     public struct Vector2
     {
         public float X { get; set; }
         public float Y { get; set; }
-        
         public static Vector2 Zero => new Vector2 { X = 0, Y = 0 };
-        
         public Vector2 Normalized()
         {
             var length = (float)Math.Sqrt(X * X + Y * Y);
